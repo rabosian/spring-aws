7c3481cfe7808ddcf8a799ff5d4e847c70bc533a
rabosian <msann1356@gmail.com>
2022-10-10 18:35:49 -0400

    4.work on views

--- src/main/java/org/example/springbootaws/web/IndexController.java
+++ src/main/java/org/example/springbootaws/web/IndexController.java
@@ -1,8 +1,10 @@
 package org.example.springbootaws.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
     @GetMapping("/")
@@ -11,5 +13,10 @@ public class IndexController {
         // mustache starter automatically sets the path and file extension as:
         // src/main/resources/templates/(filename).mustache, in this case, index.mustache
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
